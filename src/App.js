c37101398ca5ab9d3a44fc708b2c310984fce15d
partisi <erolmb@gmail.com>
2022-06-08 16:21:10 -0400

    clean sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 import "./index.scss"
 
 import Intro from "./sections/intro/Intro"
+import Contact from "./sections/contact/Contact"
+import Projects from "./sections/projects/Projects"
+import Research from "./sections/research/Research"
+import Skills from "./sections/skills/Skills"
+import Work from "./sections/work/Work"
+
 
 function App() {
   return (
@@ -10,14 +16,12 @@ function App() {
 
       <article>
         <Intro />
-
-
-
-
+        <Work />
+        <Projects />
+        <Research />
+        <Skills />
+        <Contact />
       </article>
-
-
-
     </div>
   )
 }
