dc1e9c46f230a466aacf435249ccfd931a38ca82
partisi <erolmb@gmail.com>
2022-08-22 10:52:03 -0400

    updated title

--- src/sections/intro/Intro.js
+++ src/sections/intro/Intro.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MySketch from './MySketch'
+import TextTransition, { presets } from "react-text-transition"
 
 // Introduction with animation
 export default function Intro() {
+    const TEXTS = [
+        "Fullstack Development",
+        "Blockchain",
+        "Proptech",
+        "Fintech",
+        "Venture Capital",
+        "Cloud Engineer"
+    ];
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const intervalId = setInterval(() =>
+            setIndex(index => index + 1),
+            5000
+        );
+        return () => clearTimeout(intervalId);
+    }, [])
     return (
         <section id="intro-container" className="section-container">
             <div id="intro-text">
                 <h1>Erol Bickici</h1>
-                <p>Technology, Finance, and All That in Between</p>
+                <p><span id="software-title">Software</span> and <span id="finance-title">Finance</span></p>
+                <TextTransition springConfig={presets.wobbly}>
+                    <p className="text-shuffle"> {TEXTS[index % TEXTS.length]}</p>
+                </TextTransition>
             </div>
             {/* Main Sketch Import (the star animation) */}
             <MySketch />
